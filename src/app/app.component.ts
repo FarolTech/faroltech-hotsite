f2f9e6169b3e40c9a378493fe2c12afca027cb19
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-01-29 20:11:47 -0200

    Adjusting favicons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+    if (document.getElementById("script1")) {
+      document.getElementById("script1").remove();
+    }
+    var script1 = document.createElement("script");
+    script1.setAttribute("id", "script1");
+    script1.setAttribute("src", "../assets/js/creative.js");
+    document.body.appendChild(script1);
+  }
 }
